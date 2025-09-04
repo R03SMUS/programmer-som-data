ce741ab267cd4bb4187d1409961bdc05656143fb
r03smus <rasmus06111@gmail.com>
2025-09-04 11:08:38 +0200

    afleverings mappe

---
+++ Lectures/Lec01/aflevering/Intro2.fs
@@ -0,0 +1,125 @@
+(* Programming language concepts for software developers, 2010-08-28 *)
+
+(* Evaluating simple expressions with variables *)
+
+module Intro2
+
+(* Association lists map object language variables to their values *)
+
+let env = [("a", 3); ("c", 78); ("baf", 666); ("b", 111)];;
+
+let emptyenv = [];; (* the empty environment *)
+
+let rec lookup env x =
+    match env with
+    | []        -> failwith (x + " not found")
+    | (y, v)::r -> if x=y then v else lookup r x;;
+
+let cvalue = lookup env "c";;
+
+
+(* Object language expressions with variables *)
+
+type expr =
+  | CstI of int
+  | Var of string
+  | Prim of string * expr * expr
+  | If of expr * expr * expr
+
+(*1.2 AEXPR*)
+type aexpr = 
+  | CstI of int
+  | Var of string 
+  | Add of aexpr * aexpr
+  | Mul of aexpr * aexpr
+  | Sub of aexpr * aexpr
+
+(* 1.2.2 *)
+let opgave1 = Sub(Var "v", Add(Var "w", Var "z"))
+let opgave2 = Mul(CstI 2, Sub(Var "v", Add(Var "w", Var "z")))
+let opgave3 = Add(Var "x", Add(Var "y", Add(Var "z", Var "v")))
+
+
+(* 1.2 FORMAT *)
+let rec fmt input = 
+    match input with
+    | CstI i -> string i
+    | Var x -> x
+    | Add (a1, a2) -> $"({fmt a1} + {fmt a2})" 
+    | Sub (a1, a2) -> $"({fmt a1} - {fmt a2})" 
+    | Mul (a1, a2) -> $"({fmt a1} * {fmt a2})" 
+
+(* 1.2 SIMPLIFY *)
+let rec simplify input = 
+    match input with
+    | CstI _ -> input
+    | Var _ -> input
+    | Add(CstI 0, a) | Add(a, CstI 0) -> simplify a
+    | Sub(a, CstI 0) -> simplify a
+    | Mul(CstI 1, a) | Mul (a, CstI 1) -> simplify a
+    | Mul(CstI 0, a) | Mul (a, CstI 0) -> CstI 0
+    | Sub(a,b) when a = b -> CstI 0
+    | Add(a,b) -> Add(simplify a, simplify b)
+    | Sub(a,b) -> Sub(simplify a, simplify b)
+    | Mul(a,b) -> Mul(simplify a, simplify b)
+let fejl = Add(Add(CstI 10, CstI 0), CstI 100)
+
+(* 1.2 SYMBOLIC DIFFERENCE*)
+let rec diff apexr1 aexpr2 =
+    match aexpr1, apexr2 with
+    | _, CstI a -> CstI 0
+    | Var x, Var x -> CstI 1
+    | Var x, Var y -> CstI 0 
+    | _, Add(e1, e2) ->  Add(diff apexr1 e1, diff apexr1 e2)
+    | _, Sub(e1, e2) ->  Sub(diff apexr1 e1, diff apexr1 e2)
+    | _, Mul(e1, e2) ->  Add(Mul(diff apexr1 e1, e2), Mul(diff apexr1 e2, e1))
+
+let x = fmt opgave2
+
+let e1 = CstI 17;;
+
+let e2 = Prim("+", CstI 3, Var "a");;
+
+let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a");;
+
+
+(*1.1*)
+    (* max min == *)
+let max1 = Prim("max", CstI 100, CstI 11)
+let max2 = Prim("max", CstI 100, CstI 1001)
+let min = Prim("min", CstI 20, CstI 11)
+let min = Prim("min", CstI 20, CstI 110)
+let equals1 = Prim("==", CstI 11, CstI 11)
+let equals2 = Prim("==", CstI 101, CstI 11)
+    (* max min == *)
+
+let ifeval = If(Prim("==", CstI 10, CstI 12), CstI 123, CstI 321)
+
+(* Evaluation within an environment *)
+
+
+(* 1.1 MAX, MIN, ==, IF*)
+let rec eval e (env : (string * int) list) : int =
+    match e with
+    | CstI i            -> i
+    | Var x             -> lookup env x
+    | Prim(opt, e1, e2) -> 
+        let i1 = eval e1 env
+        let i2 = eval e2 env
+        match opt with
+        | "+" -> i1 + i2
+        | "-" -> i1 - i2
+        | "*" -> i1 * i2
+        | "max" -> if i1 > i2  then i1 else i2
+        | "min" -> if i1 < i2 then i1 else i2
+        | "==" -> if i1 = i2 then 1 else 0 
+        | _ -> failwith "unknown primitive bad! input!"
+    | If(e1, e2, e3) -> if eval e1 env > 0 then eval e2 env else eval e3 env
+
+
+
+(* 1.1 Eval*)
+let e1v  = eval e1 env;;
+let e2v1 = eval e2 env;;
+let e2v2 = eval e2 [("a", 314)];;
+let e3v  = eval e3 env;;
---
+++ Lectures/Lec01/aflevering/SimpleExpr.java
@@ -0,0 +1,209 @@
+// File Intro/SimpleExpr.java
+// Java representation of expressions as in lecture 1
+// sestoft@itu.dk * 2010-08-29
+
+import java.util.Map;
+import java.util.HashMap;
+
+abstract class Expr { 
+  abstract public int eval(Map<String,Integer> env);
+  abstract public String fmt();
+  abstract public String fmt2(Map<String,Integer> env);
+  abstract public Expr simplify();
+}
+
+class CstI extends Expr { 
+  protected final int i;
+
+  public CstI(int i) { 
+    this.i = i; 
+  }
+
+  public int eval(Map<String,Integer> env) {
+    return i;
+  } 
+
+  public String fmt() {
+    return ""+i;
+  }
+
+  public String fmt2(Map<String,Integer> env) {
+    return ""+i;
+  }
+
+    @Override
+    public Expr simplify() {
+        return this;
+    }
+
+
+}
+
+class Var extends Expr { 
+  protected final String name;
+
+  public Var(String name) { 
+    this.name = name; 
+  }
+
+  public int eval(Map<String,Integer> env) {
+    return env.get(name);
+  } 
+
+  public String fmt() {
+    return name;
+  } 
+
+  public String fmt2(Map<String,Integer> env) {
+    return ""+env.get(name);
+  }
+
+    @Override
+    public Expr simplify() {
+        return this;
+    }
+
+}
+
+class Prim extends Expr { 
+  protected final String oper;
+  protected final Expr e1, e2;
+
+  public Prim(String oper, Expr e1, Expr e2) { 
+    this.oper = oper; this.e1 = e1; this.e2 = e2;
+  }
+
+  public int eval(Map<String,Integer> env) {
+    if (oper.equals("+"))
+      return e1.eval(env) + e2.eval(env);
+    else if (oper.equals("*"))
+      return e1.eval(env) * e2.eval(env);
+    else if (oper.equals("-"))
+      return e1.eval(env) - e2.eval(env);
+    else
+      throw new RuntimeException("unknown primitive");
+  } 
+
+  public String fmt() {
+    return "(" + e1.fmt() + oper + e2.fmt() + ")";
+  } 
+
+  public String fmt2(Map<String,Integer> env) {
+    return "(" + e1.fmt2(env) + oper + e2.fmt2(env) + ")";
+  }
+
+    @Override
+    public Expr simplify() {
+        return null;
+    }
+
+
+}
+
+abstract class Binop extends Prim {
+    public Binop(String oper, Expr e1, Expr e2) {
+        super(oper, e1, e2);
+    }
+    public String toString() {
+        return "(" + e1.fmt() + oper +  e2.fmt() + ")";
+    }
+}
+
+class Add extends Binop {
+
+    public Add(Expr e1, Expr e2) {
+        super("+", e1, e2);
+    }
+
+    @Override
+    public Expr simplify() {
+
+        if (e1 instanceof CstI && e2 instanceof CstI) {
+            if(((CstI) e1).i == 0)
+                return e2.simplify();
+            else if(((CstI) e2).i == 0)
+                return e1.simplify();
+        } else if (e1 instanceof CstI) {
+            if (((CstI) e1).i == 0)
+                return e2.simplify();
+        } else if (e2 instanceof CstI) {
+            if (((CstI) e2).i == 0)
+                return e1.simplify();
+        }
+        return new Add(e1.simplify(), e2.simplify());
+
+
+    }
+}
+
+class Sub extends Binop {
+
+    public Sub(Expr e1, Expr e2) {
+        super("-", e1, e2);
+    }
+
+    @Override
+    public Expr simplify() {
+        if (e2 instanceof CstI) {
+            if (((CstI) e2).i == 0)
+                return e1.simplify();
+        }
+        if (e1 instanceof Var && e2 instanceof Var) {
+            if(((Var) e1).name.equals(((Var) e2).name))
+                return new CstI(0);
+        }
+        return new Sub(e1.simplify(), e2.simplify());
+    }
+}
+
+class Mul extends Binop {
+
+    public Mul(Expr e1, Expr e2) {
+        super("*", e1, e2);
+    }
+
+    @Override
+    public Expr simplify() {
+        if (e1 instanceof CstI) {
+            if (((CstI) e1).i == 1)
+                return e2.simplify();
+            else if (((CstI) e1).i == 0)
+                return new CstI(0);
+        } else if (e2 instanceof CstI) {
+            if (((CstI) e2).i == 1)
+                return e1.simplify();
+            else if (((CstI) e2).i == 0)
+                return new CstI(0);
+        }
+        return new Mul(e1.simplify(), e2.simplify());
+    }
+}
+
+
+public class SimpleExpr {
+  public static void main(String[] args) {
+    Expr e1 = new CstI(17);
+    Expr e2 = new Prim("+", new CstI(3), new Var("a"));
+    Expr e3 = new Prim("+", new Prim("*", new Var("b"), new CstI(9)), 
+		            new Var("a"));
+    Map<String,Integer> env0 = new HashMap<String,Integer>();
+    env0.put("a", 3);
+    env0.put("c", 78);
+    env0.put("baf", 666);
+    env0.put("b", 111);
+
+    System.out.println("Env: " + env0.toString());
+
+    System.out.println(new Add(e1, e2));
+    System.out.println(new Sub(e1, e2));
+    System.out.println(new Mul(e1, e2));
+
+
+    System.out.println(new Add(new Add(new Var("x"), new CstI(0)), new CstI(0)).simplify().fmt());
+    System.out.println(new Sub(new Var("x"), new Var("y")).simplify().fmt());
+    System.out.println(new Mul(new Var("x"), new CstI(1)).simplify().fmt());
+    System.out.println(e1.fmt() + " = " + e1.fmt2(env0) + " = " + e1.eval(env0));
+    System.out.println(e2.fmt() + " = " + e2.fmt2(env0) + " = " + e2.eval(env0));
+    System.out.println(e3.fmt() + " = " + e3.fmt2(env0) + " = " + e3.eval(env0));
+  }
+}
