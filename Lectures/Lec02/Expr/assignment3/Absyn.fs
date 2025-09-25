1eb382a6fb598fdc5a0432bc3b2fd40cc7a3e8a1
r03smus <rasmus06111@gmail.com>
2025-09-25 11:27:57 +0200

    4.3

---
+++ Lectures/Lec02/Expr/assignment3/Absyn.fs
@@ -0,0 +1,13 @@
+(* File Expr/Absyn.fs
+   Abstract syntax for the simple expression language 
+ *)
+
+module Absyn
+
+type expr = 
+  | CstI of int
+  | Var of string
+  | Let of string * expr * expr
+  | Prim of string * expr * expr
+// 3.7
+  | If of expr * expr * expr
--- Lectures/Lec04/Fun/Absyn.fs
+++ Lectures/Lec04/Fun/Absyn.fs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 (* Fun/Absyn.fs * Abstract syntax for micro-ML, a functional language *)
 
 module Absyn
@@ -11,3 +12,18 @@ type expr =
   | If of expr * expr * expr
   | Letfun of string * string * expr * expr    (* (f, x, fBody, letBody) *)
   | Call of expr * expr
+=======
+(* Fun/Absyn.fs * Abstract syntax for micro-ML, a functional language *)
+
+module Absyn
+
+type expr = 
+  | CstI of int
+  | CstB of bool
+  | Var of string
+  | Let of string * expr * expr
+  | Prim of string * expr * expr
+  | If of expr * expr * expr
+  | Letfun of string * string list * expr * expr    (* (f, x list, fBody, letBody) *)
+  | Call of expr * expr list (* (Var f, eArgs list) *)
+>>>>>>> 2923e2e (4.3)
