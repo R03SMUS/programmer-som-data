8d3974b53d96814b5fc41672418370f0ee26e077
r03smus <rasmus06111@gmail.com>
2025-10-02 11:53:13 +0200

    5.1 5.7 6.1

---
+++ Lectures/Lec06/merge.java
@@ -0,0 +1,13 @@
+public class merge {
+    public static void main(String[] args) {
+        int[] l1 = {1,2,3};
+        int[] l2 = {1,2,3};
+        int[] l3 = merge(l1, l2);
+        System.out.println(l3);
+    }
+    
+    public static int[] merge(int[] l1, int[] l2) {
+        int length = l1.length + l2.length
+        int[4] i;
+    }
+}
