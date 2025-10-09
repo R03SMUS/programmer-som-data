0858f7997f7397010b7257a8886e233cdcafb675
r03smus <rasmus06111@gmail.com>
2025-10-09 12:39:19 +0200

    6.2

--- Lectures/Lec05/Fun/HigherFun.fs
+++ Lectures/Lec05/Fun/HigherFun.fs
@@ -30,6 +30,7 @@ let rec lookup env x =
 type value = 
   | Int of int
   | Closure of string * string * expr * value env       (* (f, x, fBody, fDeclEnv) *)
+  | Clos of string * expr * value env
 
 let rec eval (e : expr) (env : value env) : value =
     match e with
@@ -37,35 +38,38 @@ let rec eval (e : expr) (env : value env) : value =
     | CstB b -> Int (if b then 1 else 0)
     | Var x  -> lookup env x
     | Prim(ope, e1, e2) -> 
-      let v1 = eval e1 env
-      let v2 = eval e2 env
-      match (ope, v1, v2) with
-      | ("*", Int i1, Int i2) -> Int (i1 * i2)
-      | ("+", Int i1, Int i2) -> Int (i1 + i2)
-      | ("-", Int i1, Int i2) -> Int (i1 - i2)
-      | ("=", Int i1, Int i2) -> Int (if i1 = i2 then 1 else 0)
-      | ("<", Int i1, Int i2) -> Int (if i1 < i2 then 1 else 0)
-      |  _ -> failwith "unknown primitive or wrong type"
+        let v1 = eval e1 env
+        let v2 = eval e2 env
+        match (ope, v1, v2) with
+        | ("*", Int i1, Int i2) -> Int (i1 * i2)
+        | ("+", Int i1, Int i2) -> Int (i1 + i2)
+        | ("-", Int i1, Int i2) -> Int (i1 - i2)
+        | ("=", Int i1, Int i2) -> Int (if i1 = i2 then 1 else 0)
+        | ("<", Int i1, Int i2) -> Int (if i1 < i2 then 1 else 0)
+        | _ -> failwith "unknown primitive or wrong type"
     | Let(x, eRhs, letBody) -> 
-      let xVal = eval eRhs env
-      let letEnv = (x, xVal) :: env 
-      eval letBody letEnv
+        let xVal = eval eRhs env
+        let letEnv = (x, xVal) :: env 
+        eval letBody letEnv
     | If(e1, e2, e3) -> 
-      match eval e1 env with
-      | Int 0 -> eval e3 env
-      | Int _ -> eval e2 env
-      | _     -> failwith "eval If"
+        match eval e1 env with
+        | Int 0 -> eval e3 env
+        | Int _ -> eval e2 env
+        | _ -> failwith "eval If"
     | Letfun(f, x, fBody, letBody) -> 
-      let bodyEnv = (f, Closure(f, x, fBody, env)) :: env
-      eval letBody bodyEnv
+        let bodyEnv = (f, Closure(f, x, fBody, env)) :: env
+        eval letBody bodyEnv
     | Call(eFun, eArg) -> 
-      let fClosure = eval eFun env  (* Different from Fun.fs - to enable first class functions *)
-      match fClosure with
-      | Closure (f, x, fBody, fDeclEnv) ->
-        let xVal = eval eArg env
-        let fBodyEnv = (x, xVal) :: (f, fClosure) :: fDeclEnv
-        in eval fBody fBodyEnv
-      | _ -> failwith "eval Call: not a function";;
+        let fClosure = eval eFun env (* Different from Fun.fs - to enable first class functions *)
+        match fClosure with
+        | Closure (f, x, fBody, fDeclEnv) ->
+            let xVal = eval eArg env
+            let fBodyEnv = (x, xVal) :: (f, fClosure) :: fDeclEnv
+            eval fBody fBodyEnv
+        | _ -> failwith "eval Call: not a function"
+    | Fun (x, body) -> Clos(x, body, env)
+
+
 
 (* Evaluate in empty environment: program must have no free variables: *)
 
